f19d5440b0045d16bc2b1ad6528d47ae8d2e40c6
Sheryl Boughton <sherpbo@gmail.com>
2015-07-11 13:42:50 -0700

    adds a tester animation on intro

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,5 +1,18 @@
 console.log('Griftr online!!');
 
 $(document).ready(function(){
-      $('.light').textillate({ in: { effect: 'rollIn' } });
+  $('.light').textillate({ in: { effect: 'rollIn' } });
+
+  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+  function animateIntro (cb){
+    $("#intro").addClass("animated bounceInRight").one(animationEnd, function(){
+      $(this).removeClass("animated bounceInRight").addClass("animated slideOutUp");
+      cb();
+    });
+  }
+  animateIntro(hide);
+  function hide(){
+    $("#intro").css("display", "none");
+  }
 });
