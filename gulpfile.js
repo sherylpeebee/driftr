05676e88ca22cfc45664380dad741c5d7a1d1292
warlyware <danward@gmail.com>
2015-07-10 19:58:56 -0700

    Add gulpfile, mongodb

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-// var livereload = require()
+var gulp = require("gulp");
+var concat = require("gulp-concat");
 
-gulp.task('js', function() {
-  gulp.src('./source/javascripts/**/*.js')
-  .pipe(concat('bundle.js'))
-  .pipe(gulp.dest('./public/javascripts/'))
-  // .pipe(livereload())
+gulp.task("default", function() {
+  gulp.watch("./source/javascripts/**/*.js", ["js"]);
 });
 
-
-gulp.task('default', function() {
-  gulp.watch('./source/javascripts/**/*.js', ['js']);
+gulp.task("js", function() {
+  gulp.src("./source/javascripts/**/*.js")
+  .pipe(concat("bundle.js"))
+  .pipe(gulp.dest("./public/javascripts/"))
 });
