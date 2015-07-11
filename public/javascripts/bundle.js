df74f8c5412e547dd72eed5b4a0d9c0cf30e3bcf
Sheryl Boughton <sherpbo@gmail.com>
2015-07-11 15:10:04 -0700

    hide question, push up right image, hide suggestions

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -4,22 +4,30 @@ var app = angular.module('GriftrApp', ['ui.router']);
 
 app.run(function(){
   console.log('Griftr Online');
+
+
   $(document).ready(function(){
     $('.light').textillate({ in: { effect: 'rollIn' } });
 
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-    function animateIntro (cb){
-      $("#intro").addClass("animated bounceInRight").one(animationEnd, function(){
-        $(this).removeClass("animated bounceInRight").addClass("animated slideOutUp");
-        cb();
-      });
+    function animateIntro (){
+        $("#intro").addClass("animated bounceInRight").one(animationEnd, function(){
+          $(this).removeClass("animated bounceInRight").addClass("animated slideOutUp").one(animationEnd, function(){
+            $("#right-banner").animate({ top: -126 });
+          });
+        });
+      // $("#intro").textillate({ in: {effect: "rollIn"} , out: {effect: "rollOut"} });
+      // cb();
     }
-    animateIntro(hide);
+      animateIntro();
+
     function hide(){
       $("#intro").css("display", "none");
     }
-});
+  });
+
+
 });
 
 'use strict()';
