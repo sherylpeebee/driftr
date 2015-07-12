cf28458c90452ce363d101122fba417e6edf0608
Sheryl Boughton <sherpbo@gmail.com>
2015-07-12 02:00:25 -0700

    add rootscope to track logged in user on all pages

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -51,12 +51,12 @@ angular.module('GriftrApp')
 });
 
 angular.module('GriftrApp')
-.controller("InfoCtrl", function($scope){
+.controller("InfoCtrl", function($scope, $rootScope){
   console.log("info here!!!");
   $scope.test = 43;
 $scope.submitInfo = function(user){
   user.firstName = $rootScope.currentUser.twitter.displayName;
-  
+
   // $http.post("/")
 };
 });
--- source/javascripts/controllers/InfoCtrl.js
+++ source/javascripts/controllers/InfoCtrl.js
@@ -1,10 +1,10 @@
 angular.module('GriftrApp')
-.controller("InfoCtrl", function($scope){
+.controller("InfoCtrl", function($scope, $rootScope){
   console.log("info here!!!");
   $scope.test = 43;
 $scope.submitInfo = function(user){
   user.firstName = $rootScope.currentUser.twitter.displayName;
-  
+
   // $http.post("/")
 };
 });
