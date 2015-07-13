b229e6857a562aea764bbc397fd54d7d52e9ab60
Sheryl Boughton <sherpbo@gmail.com>
2015-07-13 16:29:01 -0700

    one line of code

--- source/javascripts/controllers/ProfileCtrl.js
+++ source/javascripts/controllers/ProfileCtrl.js
@@ -7,19 +7,19 @@ angular.module('GriftrApp')
   .success(function(houses){
     $scope.houses = houses;
   });
-  $scope.houses = [
-{image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
-{image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
-{image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
-{image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
-{image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14}
-];
+//   $scope.houses = [
+// {image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
+// {image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
+// {image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
+// {image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14},
+// {image: "http://www.loghouse.fi/wp-content/uploads/2011/11/log-house-5.jpg", squareFoot: 8788, bedrooms: 12, bathrooms: 14}
+// ];
 
 
   $scope.removeListing = function(index, house){
   $scope.houses.splice(index, 1);
-  console.log(house);
-  // $http.delete("/ownerProfile" )
+  // console.log(house);
+  $http.delete("/ownerProfile", house._id);
 
   };
 });
