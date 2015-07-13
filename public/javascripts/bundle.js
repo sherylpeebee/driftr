060240cb7592fa9b4df371335a22fd4a352c104f
Sheryl Boughton <sherpbo@gmail.com>
2015-07-13 15:02:34 -0700

    listings page

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -29,19 +29,6 @@ angular.module('GriftrApp')
 });
 
 angular.module('GriftrApp')
-.factory("Listing", function($http){
-  function Listing(){};
-  Listing.test = function() {
-    console.log('this is a test');
-    // return 'this is a test';
-  }
-  Listing.getListing = function(houseId){
-    console.log('house: ', houseId);
-    return $http.get("/listing/" + houseId);
-  }
-  return Listing;
-});
-angular.module('GriftrApp')
 .controller("HomeCtrl", function(){
   console.log("HOME CONTROLLLLLL!!!");
 
@@ -188,4 +175,18 @@ angular.module('GriftrApp')
     console.log(userData);
     $rootScope.currentUser = userData;
   });
+});
+
+angular.module('GriftrApp')
+.factory("Listing", function($http){
+  function Listing(){};
+  Listing.test = function() {
+    console.log('this is a test');
+    // return 'this is a test';
+  }
+  Listing.getListing = function(houseId){
+    console.log('house: ', houseId);
+    return $http.get("/listing/" + houseId);
+  }
+  return Listing;
 });
